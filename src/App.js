9167e47e93274378bf0adbf010f8863afd8e18ac
Samuli Peltonen <samuli.peltonen1@gmail.com>
2020-11-30 11:41:06 +0200

    Add customerTable to the app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import CustomerTable from './components/CustomerTable';
+
+function App() {
+  return (
+    <div className="App">
+      <CustomerTable></CustomerTable>
+    </div>
+  );
+}
+
+export default App;
